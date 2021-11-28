eb55710f42dab55502d59cbfc7a364f877c77671
andrelopes <andrelopessfla@gmail.com>
2021-11-27 21:15:07 -0300

    funçao para privatizar as rotas

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import {
     BrowserRouter as Router,
     Routes,
     Route,
+    Navigate
 } from "react-router-dom"
 
 import Login from "./pages/Login/Login"
@@ -9,15 +10,33 @@ import Home from "./pages/Home/Home"
 import Seach from "./pages/Seach/Seach"
 import All from "./pages/All/All"
 
+interface PrivateRouteProps{
+    children : any,
+}
+
+
+function PrivateRoute (props: PrivateRouteProps){
+    const email = localStorage.getItem('email')
+    const password = localStorage.getItem('password')
+    
+    return email && password ? props.children : <Navigate to="/" />;
+}
+
 
 const Rotas = () => {
    return(
        <Router>
            <Routes>
               <Route path="/" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/seach" element={<Seach />} />
-              <Route path="/all" element={<All />} />
+              <Route path="/home" element={<PrivateRoute>
+                  <Home />
+              </PrivateRoute>}/>
+              <Route path="/seach" element={<PrivateRoute>
+                  <Seach />
+              </PrivateRoute>} />
+              <Route path="/all" element={<PrivateRoute>
+                  <All />
+              </PrivateRoute>} />
            </Routes>
        </Router>
    )
