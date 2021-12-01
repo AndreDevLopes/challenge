1dba6f7931c3bab51517b338ba3a493e929f5a1c
andrelopes <andrelopessfla@gmail.com>
2021-12-01 02:41:14 -0300

    responsividade da tela que lista todos os pokes

--- src/pages/All/styles.tsx
+++ src/pages/All/styles.tsx
@@ -1,9 +1,19 @@
 import styled from "styled-components"
+import { device } from '../../ui/devices'
 
 export const RowButtons = styled.section`
-    padding-top: 20px;
-    margin-left: 4%;
+    padding: 20px 5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
     > button{
         margin-right:5px;
+    }
+
+    @media ${device.tablet}{
+        padding: 20px 0 0 0;
+        margin-left: 4%;
+        display: block;
     }
 `;
--- src/views/Cards/styles.tsx
+++ src/views/Cards/styles.tsx
@@ -1,7 +1,15 @@
 import styled from "styled-components"
+import { device } from '../../ui/devices'
 
 export const Container = styled.div`
     padding:50px;
     display: flex;
     flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+
+    @media ${device.tablet}{
+        justify-content: start;
+        align-items: flex-start;
+    }
 `;
